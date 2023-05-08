d88c8772f3e7a86df03c9d5493051285ea2623ce
Sialsdad <124737490+Sialsdad@users.noreply.github.com>
2023-05-07 20:07:53 -0700

    Add files via upload

--- visitor_count.php
+++ visitor_count.php
@@ -3,7 +3,7 @@ if (isset($_GET['count'])) {
   $count = $_GET['count'];
 
   // Replace the webhook URL with your own Discord webhook URL
-  $webhook_url = "https://discord.com/api/webhooks/your-webhook-url";
+  $webhook_url = "https://discord.com/api/webhooks/1100524825870602350/f_Dh-j7NvAGSHicdhWWFY6NUWyvEZdLynyksxuKVKIvLMHKityw1HpeUy4vSABSAKD_l";
 
   $data = array(
     "content" => "Visitor count: " . $count
