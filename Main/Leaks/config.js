73083f8560763c96f0944782f42f48f71b9ff386
Sialsdad <124737490+Sialsdad@users.noreply.github.com>
2023-05-22 18:09:35 -0700

    Delete Main/Leaks directory

--- Main/Leaks/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-
-const webhookId = "1105161141765808138";
-const webhookToken = "gXGG0tXtDGPTk6mUGP7yPfdXvqTvfQaYoLxWLbDcmt3Ii32Yi_bZyTh1V7ZKakk-eKI9";
-const webhookUrl = `https://discord.com/api/webhooks/${webhookId}/${webhookToken}`;
-
-module.exports = { webhookUrl };
